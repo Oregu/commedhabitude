3a6b837060fcc6f31c067dcf95f5775c5d2563a6
Oregu <oleg.burykin@gmail.com>
2014-02-22 15:36:13 +0400

    routes

--- hi.rb
+++ hi.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  "<ul><li><a href='/hi'>Hi page</a></li><li><a href='/lo'>Lo page</a></li><li><a href='myway'>My way</a></li></ul>"
+end
+
 get '/hi' do
   "Hello World!"
 end
