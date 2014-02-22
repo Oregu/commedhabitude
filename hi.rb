44d23ad7eb908af8ddf736f212e55cdd2ad1e5bf
Oregu <oleg.burykin@gmail.com>
2014-02-22 15:45:29 +0400

    add require

--- hi.rb
+++ hi.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+configure :production do
+  require 'newrelic_rpm'
+end
 
 get '/' do
   "<ul><li><a href='/hi'>Hi page</a></li><li><a href='/lo'>Lo page</a></li><li><a href='myway'>My way</a></li></ul>"
